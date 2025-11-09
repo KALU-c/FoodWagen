cd883d05b721fb485a0f808d7d718532368fcc93
Endekalu Zemenu <endekaluzemenu2134@gmail.com>
2025-11-09 18:29:54 +0300

    feat(ui): add WIP Order Card to Hero section and add separator component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,14 +1,43 @@
+import {
+	Card,
+	CardContent,
+	CardHeader
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Separator } from "@/components/ui/separator"
+import { Search } from "lucide-react"
+import { Button } from "@/components/ui/button"
+
 const Hero = () => {
 	return (
-		<section className="h-[628px] container-desktop bg-primary flex flex-col justify-center">
+		<section className="h-[628px] container-desktop bg-primary flex flex-col gap-8 justify-center">
 			{/* title + short short description */}
-			<div className="flex flex-col gap-[15px] text-white">
-				<h1 className="text-[88px] font-bold">Are you starving?</h1>
+			<div className="flex flex-col gap-3 text-white">
+				<h1 className="text-[clamp(2.8125rem,1.7131rem+4.8864vw,5.5rem)] font-bold leading-none">Are you starving?</h1>
 				<p className="text-[22px]">Withing a few clicks, find meals that are accessible near you</p>
 			</div>
 
 			{/* order card */}
-			<div className="flex flex-col gap-2"></div>
+			<Card className="max-w-4xl">
+				<CardHeader></CardHeader>
+				<Separator />
+				<CardContent className="flex sm:flex-row flex-col gap-6">
+					<div className="relative flex items-center rounded-[8px] focus:border focus-within:ring-1 focus-within:ring-[#F17228] pl-2 bg-[#F5F5F5] flex-1">
+						<Search className="h-[20px] w-[20px] text-[#F17228]" />
+						<Input
+							type="text"
+							name="food_order_input"
+							placeholder="What do you like to eat today?"
+							className="border-0 focus-visible:ring-0 shadow-none h-[60px] placeholder:text-lg text-lg! placeholder:text-[#9E9E9E]"
+						/>
+					</div>
+
+					<Button className="bg-linear-to-r from-[#FF7A7A] to-[#F65900] sm:h-full h-[60px] has-[>svg]:px-12 gap-2 text-lg font-bold">
+						<Search className="h-[20px] w-[20px]" strokeWidth={3} />
+						Find Meal
+					</Button>
+				</CardContent>
+			</Card>
 		</section>
 	)
 }
