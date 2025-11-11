1fe81e0dd402f140840ad465d2d9f7ba5ac32cfa
Endekalu Zemenu <endekaluzemenu2134@gmail.com>
2025-11-11 06:38:26 +0300

    feat(ui): add WIP Featured Meals component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import FeaturedMeals from "@/components/FeaturedMeals";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <main>
-      <Navbar />
-      <Hero />
+    <main className="flex flex-col gap-10">
+      <div>
+        <Navbar />
+        <Hero />
+      </div>
+      <FeaturedMeals />
     </main>
   );
 }
---
+++ components/FeaturedMeals.tsx
@@ -0,0 +1,9 @@
+const FeaturedMeals = () => {
+	return (
+		<section className="flex flex-col items-center">
+			<h2 className="text-[43px] font-bold">Featured Meals</h2>
+		</section>
+	)
+}
+
+export default FeaturedMeals
