c8979246608331248b3e4f4aceafadae4ba6d509
Endekalu Zemenu <endekaluzemenu2134@gmail.com>
2025-11-11 09:50:50 +0300

    feat: implement featured meals fetching and enhance MealCard component with dynamic props

--- components/sections/meal/FeaturedMeals.tsx
+++ components/sections/meal/FeaturedMeals.tsx
@@ -1,15 +1,22 @@
+"use client"
+
+import { useFeaturedMeals } from "@/hooks/useFeaturedMeals"
 import MealCard from "./MealCard"
 
 const FeaturedMeals = () => {
+	const { data: featuredMeals, isPending, error } = useFeaturedMeals()
+
+	if(isPending) return <p>loading...</p>
+	if(error) return <p>error</p>
+
 	return (
 		<section className="flex flex-col gap-[90px] items-center">
 			<h2 className="text-[43px] font-bold">Featured Meals</h2>
 
 			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-				<MealCard />
-				<MealCard />
-				<MealCard />
-				<MealCard />
+				{featuredMeals.map(meal => (
+					<MealCard key={meal.id} {...meal} />
+				))}
 			</div>
 		</section>
 	)
--- components/sections/meal/MealCard.tsx
+++ components/sections/meal/MealCard.tsx
@@ -1,40 +1,49 @@
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
+import { FeaturedMealType } from "@/types/featured-meals"
 import { MoreVertical, Star, Tag } from "lucide-react"
 import Image from "next/image"
 
-const MealCard = () => {
+const MealCard = ({
+	name,
+	restaurantName,
+	image,
+	rating,
+	Price,
+	logo,
+	status
+}: FeaturedMealType) => {
 	return (
 		<Card className="p-0 gap-[28px] min-w-[340px] max-w-[357px] border-none shadow-none">
 			<CardHeader className="relative p-0 h-[301px] rounded-2xl overflow-hidden gap-0">
 				<Image
-					alt="" // TODO - add meal name
+					alt={name}
 					width={340}
 					height={301}
-					src={'https://images.unsplash.com/photo-1504674900247-0877df9cc836?auto=format&fit=crop&w=800&q=80'}
+					src={image}
 					className="absolute h-full object-cover transition-transform duration-500 hover:scale-105"
 					unoptimized
 				/>
 
 				<div className="absolute top-4 left-4 bg-[#F17228] px-4 py-2 flex items-center gap-[10px] rounded-[8px]">
 					<Tag className="size-5 fill-white text-[#F17228]" />
-					<p className="font-bold text-[22px] leading-none text-white">$2.99</p>
+					<p className="font-bold text-[22px] leading-none text-white">${Price}</p>
 				</div>
 			</CardHeader>
 			<CardContent className="p-0 flex justify-between">
 				<div className="flex gap-6">
 					<Image
-						src={'https://images.unsplash.com/photo-1522771930-78848d9293e8?auto=format&fit=crop&w=120&q=60'}
-						alt="" // TODO
+						src={logo}
+						alt={restaurantName}
 						height={64}
 						width={64}
 						className="h-16 w-16 rounded-lg"
 					/>
 
 					<div className="flex flex-col gap-1">
-						<p className="text-[22px] font-bold leading-none">Bow Lasagna</p>
+						<p className="text-[22px] font-bold leading-none">{restaurantName}</p>
 						<div className="flex items-center gap-2">
 							<Star className="size-[22px] text-primary fill-primary" />
-							<p className="text-[22px] text-primary">4.6</p>
+							<p className="text-[22px] text-primary">{rating}</p>
 						</div>
 					</div>
 				</div>
@@ -42,7 +51,7 @@ const MealCard = () => {
 			</CardContent>
 			<CardFooter className="p-0">
 				<div className="px-4 py-2 bg-[#F17228]/20 rounded-2xl">
-					<p className="text-xl font-bold text-[#F17228]">Closed</p>
+					<p className="text-xl font-bold text-[#F17228]">{status}</p>
 				</div>
 			</CardFooter>
 		</Card>
